6f7bffae67c426ad969ba6867396d45c3107280a
shnider42 <holtsniderc@gmail.com>
2026-04-29 16:14:56 -0400

    Export facet option model

--- workday_jobs/__init__.py
+++ workday_jobs/__init__.py
@@ -1,11 +1,13 @@
 from .config import WorkdaySiteConfig
 from .client import WorkdayClient
+from .facets import FacetOption
 from .models import JobPosting, RankedJob
 from .ranker import KeywordRanker, default_profile
 
 __all__ = [
     "WorkdaySiteConfig",
     "WorkdayClient",
+    "FacetOption",
     "JobPosting",
     "RankedJob",
     "KeywordRanker",
