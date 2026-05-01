a1437ecbd00814c18d9821b835767db68ed12b06
shnider42 <holtsniderc@gmail.com>
2026-04-30 20:05:26 -0400

    Test Workday public URL prefix building

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,4 +1,4 @@
-from workday_jobs.parsing import clean_description, extract_json_ld, parse_req_id_from_path
+from workday_jobs.parsing import build_public_job_url, clean_description, extract_json_ld, parse_req_id_from_path
 
 
 def test_parse_req_id_from_path():
@@ -21,3 +21,13 @@ def test_clean_description_can_trim_start_and_stop_cues():
 def test_extract_json_ld():
     html = '<script type="application/ld+json">{"@type":"JobPosting","title":"Engineer"}</script>'
     assert extract_json_ld(html)["title"] == "Engineer"
+
+
+def test_build_public_job_url_uses_public_path_prefix():
+    url = build_public_job_url(
+        "https://wd1.myworkdaysite.com",
+        "FidelityCareers",
+        "/job/Boston-MA/Principal-Engineer_2099779",
+        public_path_prefix="/en-US/recruiting/fmr/FidelityCareers",
+    )
+    assert url == "https://wd1.myworkdaysite.com/en-US/recruiting/fmr/FidelityCareers/job/Boston-MA/Principal-Engineer_2099779"
