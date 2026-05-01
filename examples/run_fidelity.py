a60598a17df5c94b10499f81201fd9f36b3c9d9f
shnider42 <holtsniderc@gmail.com>
2026-04-30 20:05:40 -0400

    Add Fidelity Workday example runner

---
+++ examples/run_fidelity.py
@@ -0,0 +1,12 @@
+from workday_jobs import KeywordRanker, WorkdayClient, WorkdaySiteConfig
+
+config = WorkdaySiteConfig.from_public_url(
+    "https://wd1.myworkdaysite.com/en-US/recruiting/fmr/FidelityCareers"
+)
+
+client = WorkdayClient(config)
+jobs = client.discover_jobs(max_pages=10, max_jobs=100, hydrate=True)
+ranked = KeywordRanker().rank(jobs)
+
+for item in ranked[:25]:
+    print(f"{item.score:>6} | {item.job.title} | {item.job.location} | {item.job.url}")
