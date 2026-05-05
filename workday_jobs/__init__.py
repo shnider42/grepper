2d99c4a3690ebd18f0892c0001d7a8c9a4ecbed8
shnider42 <holtsniderc@gmail.com>
2026-05-04 20:30:39 -0400

    Export iCIMS provider types

--- workday_jobs/__init__.py
+++ workday_jobs/__init__.py
@@ -1,15 +1,22 @@
 from .config import WorkdaySiteConfig
 from .client import WorkdayClient
 from .facets import FacetOption
+from .icims import IcimsClient, IcimsSiteConfig
 from .models import JobPosting, RankedJob
 from .ranker import KeywordRanker, default_profile
+from .sources import client_from_config, config_from_public_url, provider_name
 
 __all__ = [
     "WorkdaySiteConfig",
     "WorkdayClient",
+    "IcimsSiteConfig",
+    "IcimsClient",
     "FacetOption",
     "JobPosting",
     "RankedJob",
     "KeywordRanker",
     "default_profile",
+    "config_from_public_url",
+    "client_from_config",
+    "provider_name",
 ]
